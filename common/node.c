20ea6b6a07853d2525c4199c85c59aa9fdf112f2
Wayne Small <waynemystir@gmail.com>
2017-03-09 00:56:24 -0500

    re-work node into different structs

--- common/node.c
+++ common/node.c
@@ -5,7 +5,83 @@
 
 #include "node.h"
 
-int nodes_equal(struct node *n1, struct node *n2) {
+void addr_to_node_buf(struct sockaddr *sa, node_buf_t **nb, STATUS_TYPE status, unsigned short int_or_ext) {
+	if (!sa || !nb) return;
+
+	node_buf_t *new_node_buf = malloc(SZ_NODE_BF);
+	*nb = new_node_buf;
+	new_node_buf->status = status;
+	new_node_buf->int_or_ext = int_or_ext;
+	new_node_buf->family = sa->sa_family;
+	switch (new_node_buf->family) {
+		case AF_INET: {
+			struct sockaddr_in *sa4 = (struct sockaddr_in *)sa;
+			new_node_buf->ip4 = sa4->sin_addr.s_addr;
+			break;
+		}
+		case AF_INET6: {
+			struct sockaddr_in6 *sa6 = (struct sockaddr_in6 *)sa;
+			memcpy(new_node_buf->ip6, sa6->sin6_addr.s6_addr, sizeof(unsigned char[16]));
+			break;
+		}
+		default: break;
+	}
+}
+
+int node_buf_to_addr(node_buf_t *node_buf, struct sockaddr **addr) {
+	if (!addr || !node_buf) return -1;
+
+	switch (node_buf->family) {
+		case AF_INET: {
+			struct sockaddr_in *sai = malloc(SZ_SOCKADDR_IN);
+			sai->sin_addr.s_addr = node_buf->ip4;
+			sai->sin_port = node_buf->port;
+			sai->sin_family = AF_INET;
+			*addr = (struct sockaddr*)sai;
+			(*addr)->sa_family = AF_INET;
+			break;
+		}
+		case AF_INET6: {
+			struct sockaddr_in6 *sai = malloc(SZ_SOCKADDR_IN6);
+			memcpy(sai->sin6_addr.s6_addr, node_buf->ip6, sizeof(unsigned char[16]));
+			sai->sin6_port = node_buf->port;
+			sai->sin6_family = AF_INET;
+			*addr = (struct sockaddr*)&sai;
+			(*addr)->sa_family = AF_INET6;
+			break;
+		}
+		default: {
+			break;
+		}
+	}
+
+	return 0;
+}
+
+void node_buf_to_node_min(node_buf_t *nb, node_min_t **nm) {
+	if (!nb || !nm) return;
+
+	node_min_t *new_node_min = malloc(SZ_NODE_MN);
+	*nm = new_node_min;
+	new_node_min->next = NULL;
+	new_node_min->status = nb->status;
+	new_node_min->int_or_ext = nb->int_or_ext;
+	new_node_min->port = nb->port;
+	new_node_min->family = nb->family;
+	switch (new_node_min->family) {
+		case AF_INET: {
+			new_node_min->ip4 = nb->ip4;
+			break;
+		}
+		case AF_INET6: {
+			memcpy(new_node_min->ip6, nb->ip6, 16);
+			break;
+		}
+		default: break;
+	}
+}
+
+int nodes_min_equal(node_min_t *n1, node_min_t *n2) {
 	if (!n1 || !n2) return 0;
 	if (n1->family != n2->family) return 0;
 	if (n1->port != n2->port) return 0;
@@ -17,18 +93,18 @@ int nodes_equal(struct node *n1, struct node *n2) {
 	}
 }
 
-struct node *find_node(LinkedList *list, struct node *node) {
+node_min_t *find_node_min(LinkedList_min_t *list, node_min_t *node) {
 	if (!list || !list->head) return NULL;
 
-	struct node *p = list->head;
+	node_min_t *p = list->head;
 	while (p) {
-		if (nodes_equal(p, node)) return p;
+		if (nodes_min_equal(p, node)) return p;
 		p = p->next;
 	}
 	return NULL;
 }
 
-int node_and_sockaddr_equal(node_t *node, struct sockaddr *addr) {
+int node_min_and_sockaddr_equal(node_min_t *node, struct sockaddr *addr) {
 	if (!node || !addr) return 0;
 	if (node->family != addr->sa_family) return 0;
 
@@ -47,10 +123,88 @@ int node_and_sockaddr_equal(node_t *node, struct sockaddr *addr) {
 	}
 }
 
+node_min_t *find_node_min_from_sockaddr(LinkedList_min_t *list, struct sockaddr *addr) {
+	if (!list || !addr) return NULL;
+
+	node_min_t *p = list->head;
+	while (p) {
+		if (node_min_and_sockaddr_equal(p, addr)) return p;
+		p = p->next;
+	}
+	return NULL;
+}
+
+void add_node_min(LinkedList_min_t *list, node_min_t *node) {
+	if (!list || !node) return;
+	node->next = NULL;
+
+	if (!list->head) {
+		list->head = node;
+		list->tail = node;
+	} else {
+		list->tail->next = node;
+		list->tail = node;
+	}
+
+	list->node_count++;
+}
+
+void nodes_min_perform(LinkedList_min_t *list, void (*perform)(node_min_t *node)) {
+	if (!list || !list->head) return;
+
+	node_min_t *p = list->head;
+	while (p) {
+		perform(p);
+		p = p->next;
+	}
+}
+
+int nodes_equal(node_t *n1, node_t *n2) {
+	if (!n1 || !n2) return 0;
+	if (n1->external_family != n2->external_family) return 0;
+	if (n1->external_port != n2->external_port) return 0;
+
+	switch (n1->external_family) {
+		case AF_INET: return n1->external_ip4 == n2->external_ip4;
+		case AF_INET6: return n1->external_ip6 == n2->external_ip6;
+		default: return 0;
+	}
+}
+
+struct node *find_node(LinkedList *list, node_t *node) {
+	if (!list || !list->head) return NULL;
+
+	node_t *p = list->head;
+	while (p) {
+		if (nodes_equal(p, node)) return p;
+		p = p->next;
+	}
+	return NULL;
+}
+
+int node_and_sockaddr_equal(node_t *node, struct sockaddr *addr) {
+	if (!node || !addr) return 0;
+	if (node->external_family != addr->sa_family) return 0;
+
+	switch (addr->sa_family) {
+		case AF_INET: {
+			struct sockaddr_in *sa4 = (struct sockaddr_in *)addr;
+			return node->external_ip4 == sa4->sin_addr.s_addr &&
+				node->external_port == sa4->sin_port;
+		}
+		case AF_INET6: {
+			struct sockaddr_in6 *sa6 = (struct sockaddr_in6 *)addr;
+			return node->external_ip6 == sa6->sin6_addr.s6_addr &&
+				node->external_port == sa6->sin6_port;
+		}
+		default: return 0;
+	}
+}
+
 struct node *find_node_from_sockaddr(LinkedList *list, struct sockaddr *addr) {
 	if (!list || !list->head) return NULL;
 
-	struct node *p = list->head;
+	node_t *p = list->head;
 	while (p) {
 		if (node_and_sockaddr_equal(p, addr)) return p;
 		p = p->next;
@@ -71,10 +225,10 @@ void copy_and_add_tail(LinkedList *list, node_t *node_to_copy, node_t **new_tail
 
 	node_t *nn;
 
-	nn = malloc(sizeof(node_t));
+	nn = malloc(SZ_NODE);
 	*new_tail = nn;
-	memset(nn, '\0', sizeof(node_t));
-	memcpy(nn, node_to_copy, sizeof(node_t));
+	memset(nn, '\0', SZ_NODE);
+	memcpy(nn, node_to_copy, SZ_NODE);
 	nn->next = NULL;
 
 	if (!list->head) {
@@ -91,7 +245,7 @@ void copy_and_add_tail(LinkedList *list, node_t *node_to_copy, node_t **new_tail
 void get_new_tail(LinkedList *list, node_t **new_tail) {
 	if (!new_tail) return;
 	node_t ntc;
-	memset(&ntc, '\0', sizeof(node_t));
+	memset(&ntc, '\0', SZ_NODE);
 	copy_and_add_tail(list, &ntc, new_tail);
 }
 
--- common/node.h
+++ common/node.h
@@ -20,8 +20,9 @@ typedef enum STATUS_TYPE {
     STATUS_CHAT_PORT = 7,
 } STATUS_TYPE;
 
-typedef struct node {
+typedef struct node_buf {
 	STATUS_TYPE status;
+	unsigned short int_or_ext; // 0 is internal and 1 is external
 	union {
 		unsigned long ip4;
 		unsigned char ip6[16];
@@ -29,12 +30,46 @@ typedef struct node {
 	unsigned short port;
 	unsigned short chat_port;
 	unsigned short family;
+} node_buf_t;
+
+typedef struct node_min {
+	STATUS_TYPE status;
+	unsigned short int_or_ext; // 0 is internal and 1 is external
+	union {
+		unsigned long ip4;
+		unsigned char ip6[16];
+	};
+	unsigned short port;
+	unsigned short chat_port;
+	unsigned short family;
+	struct node_min *next;
+} node_min_t;
+
+typedef struct node {
+	STATUS_TYPE status;
+	unsigned short int_or_ext; // 0 is internal and 1 is external
+	union {
+		unsigned long internal_ip4;
+		unsigned char internal_ip6[16];
+	};
+	unsigned short internal_port;
+	unsigned short internal_chat_port;
+	unsigned short internal_family;
+	union {
+		unsigned long external_ip4;
+		unsigned char external_ip6[16];
+	};
+	unsigned short external_port;
+	unsigned short external_chat_port;
+	unsigned short external_family;
 	struct node *next;
 } node_t;
 
-// TODO I want to minimize network buffer: Remove next from node
-// and make separate node_internal that includes next. Then add
-// function to switch between node and node_internal and voila.
+typedef struct LinkedList_min {
+	node_min_t *head;
+	node_min_t *tail;
+	int node_count;
+} LinkedList_min_t;
 
 typedef struct LinkedList {
 	node_t *head;
@@ -42,9 +77,40 @@ typedef struct LinkedList {
 	int node_count;
 } LinkedList;
 
-int nodes_equal(struct node *n1, struct node *n2);
+#define SZ_NODE_BF sizeof(node_buf_t)
+#define SZ_NODE_MN sizeof(node_min_t)
+#define SZ_NODE sizeof(node_t)
+#define SZ_LINK_LIST_MN sizeof(LinkedList_min_t)
+#define SZ_LINK_LIST sizeof(LinkedList)
+#define SZ_SOCKADDR sizeof(struct sockaddr)
+#define SZ_SOCKADDR_IN sizeof(struct sockaddr_in)
+#define SZ_SOCKADDR_IN6 sizeof(struct sockaddr_in6)
 
-struct node *find_node(LinkedList *list, struct node *node);
+// LinkedList_min and node_min_t functions
+
+void addr_to_node_buf(struct sockaddr *sa, node_buf_t **nb, STATUS_TYPE status, unsigned short int_or_ext);
+
+int node_buf_to_addr(node_buf_t *node_buf, struct sockaddr **addr);
+
+void node_buf_to_node_min(node_buf_t *nb, node_min_t **nm);
+
+int nodes_min_equal(node_min_t *n1, node_min_t *n2);
+
+node_min_t *find_node_min(LinkedList_min_t *list, node_min_t *node);
+
+int node_min_and_sockaddr_equal(node_min_t *node, struct sockaddr *addr);
+
+node_min_t *find_node_min_from_sockaddr(LinkedList_min_t *list, struct sockaddr *addr);
+
+void add_node_min(LinkedList_min_t *list, node_min_t *node);
+
+void nodes_min_perform(LinkedList_min_t *list, void (*perform)(node_min_t *node));
+
+// LinkedList and node_t functions
+
+int nodes_equal(node_t *n1, node_t *n2);
+
+struct node *find_node(LinkedList *list, node_t *node);
 
 int node_and_sockaddr_equal(node_t *node, struct sockaddr *addr);
 
